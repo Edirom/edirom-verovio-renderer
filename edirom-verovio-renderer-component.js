b4c5322aa353aeb0b4628ed5d9c1c51e10063c98
Hizkiel <hizclick@gmail.com>
2025-04-08 09:06:51 +0200

    update the component so that it can listen to attribute changes, add new attributes and update the readme

--- edirom-verovio-renderer-component.js
+++ edirom-verovio-renderer-component.js
@@ -29,11 +29,8 @@ class EdiromVerovioRenderer extends HTMLElement {
       <div id="verovio-svg">
       </div>
       <div id="verovio-header">
-          <button id="next_btn">Next</button>
-          <button id="previous_btn">Previous</button> 
-          <button id="zoom_up">+</button>
-          <button id="zoom_down">-</button> 
-          <span id="page_info">${this.pageNumber} / ${this.totalPages}</span>
+ 
+          <span id="page_info"></span>
     </body>`
 
   }
@@ -41,19 +38,26 @@ class EdiromVerovioRenderer extends HTMLElement {
     this.renderVerovio();
     this.setupResizeObserver();
 
-    this.shadowRoot.getElementById("next_btn").addEventListener('click', () => this.calculatePageNumber('next'));
-    this.shadowRoot.getElementById("previous_btn").addEventListener('click', () => this.calculatePageNumber('previous'));
-    this.shadowRoot.getElementById("zoom_up").addEventListener('click', () => this.calculateZoom('zoomUp'));
-    this.shadowRoot.getElementById("zoom_down").addEventListener('click', () => this.calculateZoom('zoomDown'));
+    const btnNext = document.getElementById("btn-next");
+    const btnPrev = document.getElementById("btn-prev");
+    const btnZoomIn = document.getElementById("btn-zoom-in");
+    const btnZoomOut = document.getElementById("btn-zoom-out");
+  
+    if (btnNext) btnNext.addEventListener("click", () => this.calculatePageNumber("next"));
+    if (btnPrev) btnPrev.addEventListener("click", () => this.calculatePageNumber("previous"));
+    if (btnZoomIn) btnZoomIn.addEventListener("click", () => this.calculateZoom("zoomUp"));
+    if (btnZoomOut) btnZoomOut.addEventListener("click", () => this.calculateZoom("zoomDown"));
+   
   }
 
   static get observedAttributes() {
-    return ['zoom', 'height', 'width', 'pagenumber', 'meiurl'];
+    return ['zoom', 'height', 'width', 'pagenumber', 'meiurl', 'measurenumber', 'mdivname'];
   }
   attributeChangedCallback(property, oldValue, newValue) {
 
     // handle property change
     this.set(property, newValue);
+    console.log("property ", property , " is change from ", oldValue, " to ", newValue )
 
   }
   set(property, newPropertyValue) {
@@ -67,7 +71,103 @@ class EdiromVerovioRenderer extends HTMLElement {
       bubbles: true
     });
     this.dispatchEvent(event);
+    this.handlePropertyChange(property, newPropertyValue) 
   }
+
+  handlePropertyChange(property, newPropertyValue) {
+
+
+  switch (property) {
+    case 'zoom':
+      this.zoom = parseInt(newPropertyValue);
+      this.setupOptions();
+      this.tk?.setOptions(this.options);
+      this.renderSVG();
+      break;
+
+    case 'pagenumber':
+      this.pageNumber = parseInt(newPropertyValue);
+      this.renderSVG();
+      break;
+
+    case 'height':
+    case 'width':
+      this[property] = parseInt(newPropertyValue);
+      this.updatePageDimensions();
+      this.setupOptions();
+      this.tk?.setOptions(this.options);
+      this.renderSVG();
+      break;
+
+    case 'meiurl':
+      this.meiurl = newPropertyValue;
+      this.fetchAndRenderMEI();
+      break;
+
+    case 'measurenumber':
+      this.gotoMeasure(newPropertyValue);
+      break;
+    case 'mdivname':
+      this.mdivname = newPropertyValue;
+      console.log("mdiv name is ", newPropertyValue)
+      break;
+      
+  }
+  
+  }
+  gotoMeasure(measureNumber) {
+    const measureId = this.getMeasureIdByNumber(measureNumber);
+  
+    if (measureId) {
+      const page = this.tk.getPageWithElement(measureId);
+      if (page) {
+        this.pageNumber = page;
+        this.renderSVG();
+        console.log(`Navigated to measure ${measureNumber} on page ${page}`);
+      } else {
+        console.warn(`Page not found for measure ID: ${measureId}`);
+      }
+    } else {
+      console.warn(`Measure with n="${measureNumber}" not found`);
+    }
+  }
+  
+  
+  
+  getMeasureIdByNumber(nValue) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(this.meiData, "application/xml");
+  
+    let measures;
+  
+    if (this.mdivname) {
+      // Find the <mdiv> with the label attribute
+      const mdiv = xmlDoc.querySelector(`mdiv[label="${this.mdivname}"]`);
+      if (!mdiv) {
+        console.warn(`No <mdiv> found with label="${this.mdivname}"`);
+        return null;
+      }
+  
+      // Only search within this mdiv
+      measures = mdiv.querySelectorAll("measure");
+    } else {
+      // No mdiv specified, search all measures in the document
+      measures = xmlDoc.querySelectorAll("measure");
+    }
+  
+    // Look for a measure with matching n value
+    for (let measure of measures) {
+      if (measure.getAttribute("n") === nValue.toString()) {
+        return measure.getAttribute("xml:id"); // Verovio needs this
+      }
+    }
+  
+    console.warn(`No <measure n="${nValue}"> found ${this.mdivname ? `in <mdiv label="${this.mdivname}">` : "in the MEI file"}`);
+    return null;
+  }
+    
+  
+  
   calculateZoom(type) {
     this.zoom = parseInt(this.zoom)
     this.zoom += type === "zoomUp" ? 10 : -10;
@@ -119,6 +219,7 @@ class EdiromVerovioRenderer extends HTMLElement {
     fetch(this.meiurl)
       .then((response) => response.text())
       .then((mei) => {
+        this.meiData = mei;
         this.tk.loadData(mei);
         this.renderSVG();
       });
@@ -137,11 +238,19 @@ class EdiromVerovioRenderer extends HTMLElement {
     this.pagenumber = parseInt(this.pagenumber)
     this.pageNumber = (this.pagenumber != null && this.pagenumber !== '' && this.pagenumber >= 1 && this.pagenumber <= this.totalPages) ? this.pagenumber : this.pageNumber;
     this.pagenumber = 0
-    this.shadowRoot.getElementById("page_info").textContent = `${this.pageNumber} / ${this.totalPages}`;
 
     let svg = this.tk.renderToSVG(this.pageNumber);
     this.shadowRoot.getElementById("verovio-svg").innerHTML = svg;
     this.styleSVGElements();
+
+
+    this.dispatchEvent(new CustomEvent('page-info-update', {
+      detail: {
+        pageNumber: this.pageNumber,
+        totalPages: this.totalPages
+      },
+      bubbles: true
+    }));
   }
 
   styleSVGElements() {
