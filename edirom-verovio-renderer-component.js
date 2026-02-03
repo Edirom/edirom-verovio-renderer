298f66090b2b7ad45648d05f84bdd2fc5379099a
Daniel Jettka <research@jettka.de>
2026-02-03 11:34:08 +0100

    Revert "PR: Add Verovio Layout Options for Page and System Break Control"

--- edirom-verovio-renderer-component.js
+++ edirom-verovio-renderer-component.js
@@ -47,7 +47,7 @@ class EdiromVerovioRenderer extends HTMLElement {
     /** set global properties */
     this.veroviourl = this.getAttribute('verovio-url') || "https://www.verovio.org/javascript/5.3.2/verovio-toolkit-wasm.js";    
     this.options = this.getAttribute("verovio-options") || {
-      breaks: this.getAttribute('breakmode') || "auto",
+      breaks: "auto",
       scale: 20,
       spacingStaff: 7,
       pageHeight: 4500,
@@ -94,7 +94,7 @@ class EdiromVerovioRenderer extends HTMLElement {
    * @returns {Array<string>} The list of observed attributes.
    */
   static get observedAttributes() {
-    return ['zoom', 'height', 'width', 'pagenumber', 'meiurl', 'elementid', 'measurenumber', 'mdivname', "movementid", "pagewidth", "pageheight", "verovio-url", "verovio-options", "breakmode"];
+    return ['zoom', 'height', 'width', 'pagenumber', 'meiurl', 'elementid', 'measurenumber', 'mdivname', "movementid", "pagewidth", "pageheight", "verovio-url", "verovio-options"];
   }
 
   /**
@@ -210,27 +210,6 @@ class EdiromVerovioRenderer extends HTMLElement {
           this.tk?.setOptions(this.options);
           this.tk?.loadData(this.meiData);
           this.renderSVG();
-        }
-        break;
-
-      case 'breakmode':
-        this.options['breaks'] = newPropertyValue;
-        this.tk?.setOptions(this.options);
-        this.tk?.loadData(this.meiData);
-        this.renderSVG();
-        break;
-
-      case 'verovio-options':
-        try {
-          const newOptions = typeof newPropertyValue === 'string' 
-            ? JSON.parse(newPropertyValue) 
-            : newPropertyValue;
-          this.options = { ...this.options, ...newOptions };
-          this.tk?.setOptions(this.options);
-          this.tk?.loadData(this.meiData);
-          this.renderSVG();
-        } catch (e) {
-          console.error('Invalid verovio-options format:', e);
         }
         break;
     }
@@ -427,7 +406,7 @@ class EdiromVerovioRenderer extends HTMLElement {
    */
   renderSVG() {
     this.totalPages = this.tk?.getPageCount();
-    this.pageNumber = (!isNaN(this.pageNumber) && !isNaN(this.totalPages) && this.pageNumber >= 1 && this.pageNumber <= this.totalPages) ? this.pageNumber : 1;
+    this.pageNumber = (!isNaN(this.pageNumber) && !isNaN(this.totalPages) && this.pageNumber >= 1 && this.pagenumber <= this.totalPages) ? this.pageNumber : 1;
 
     let svg = this.tk?.renderToSVG(this.pageNumber);
     this.shadowRoot.getElementById("verovio-svg").innerHTML = svg;
